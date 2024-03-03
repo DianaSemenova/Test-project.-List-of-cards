0de75a0631c2bb09f5d0cdcdda05841ba456ea68
DianaSemenova <menestrelia199702@gmail.com>
2024-03-03 17:39:42 +0400

    added global.style.js, installed react-router-dom, styled-components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes/Routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <BrowserRouter>
+            <AppRoutes />
+        </BrowserRouter>
+    );
 }
-
-export default App;
